cf4cb0ee0a93c19674730b8ba6fcc499893033f6
Diego Doná <diedona@gmail.com>
2020-08-15 12:29:34 -0300

    add tests

---
+++ VendaDDD.Tests/Entities/VendaTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace VendaDDD.Tests.Entities
+{
+    public class VendaTests
+    {
+        [Fact]
+        public void TesteQueFunciona()
+        {
+            Assert.Equal(4, 4);
+        }
+    }
+}
