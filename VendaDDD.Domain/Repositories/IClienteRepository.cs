73f2818aeda6c9b9e1069b2b8bea1d6d10cc83ce
Diego Doná <diedona@gmail.com>
2020-08-15 19:09:09 -0300

    interfaces de repositories

---
+++ VendaDDD.Domain/Repositories/IClienteRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VendaDDD.Domain.Entities;
+
+namespace VendaDDD.Domain.Repositories
+{
+    public interface IClienteRepository
+    {
+        Cliente CarregarCliente(Guid id);
+    }
+}
---
+++ VendaDDD.Domain/Repositories/IVendedorRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VendaDDD.Domain.Entities;
+
+namespace VendaDDD.Domain.Repositories
+{
+    public interface IVendedorRepository
+    {
+        Vendedor CarregarVendedor(Guid id);
+    }
+}
