667e22d7b6bd3ade3ed988f266c7c58a59bdff06
Diego Doná <diedona@gmail.com>
2020-08-17 20:59:17 -0300

    adicionando bounded context de segurança

---
+++ SegurancaBC.Domain/Entities/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using SharedKernel.Entities;
+using SharedKernel.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegurancaBC.Domain.Entities
+{
+    public class Usuario : Entity
+    {
+        public Email NomeDeUsuario { get; private set; }
+        public bool Ativo { get; private set; }
+
+        public Usuario(Email nomeDeUsuario, Guid? id = null) : base(id)
+        {
+            NomeDeUsuario = nomeDeUsuario;
+            Ativo = false;
+        }
+    }
+}
